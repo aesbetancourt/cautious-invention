ef442f73c8e34413fe89dfbc71f095cace53fb03
Alejandro <aesbetancourt@outlook.com>
2019-07-27 12:35:35 -0400

    added new components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,21 @@
 </template>
 
 <script>
-
-
-
 // Components
 import Start from './components/Start';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 export default {
   name: 'App',
   components: {
-    Start,
-    Signup,
-    Login,
-    Home
+      Start,
+      Signup,
+      Login,
+      Home,
+      Profile
   }
 }
 </script>
