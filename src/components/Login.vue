4f1a22bd8802b2cf12171a870b2a8081442e75b5
Alejandro <aesbetancourt@outlook.com>
2019-07-28 15:26:29 -0400

    Little changes
    linked fonts awesome in html and deleted "!" in login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
         <h2 class="text-center"><strong>Inicia</strong>&nbsp;sesión.</h2>
         <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" v-model="email"></div>
         <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Contraseña" v-model="password"></div>
-        <div class="form-group"><button class="btn btn-primary btn-block" type="submit" style="background-color: rgb(37,55,102);">Iniciar Sesion!</button></div>
+        <div class="form-group"><button class="btn btn-primary btn-block" type="submit" style="background-color: rgb(37,55,102);">Iniciar Sesion</button></div>
       </form>
     </div>
   </div>
